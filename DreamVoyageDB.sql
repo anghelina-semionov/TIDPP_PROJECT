cef540ecaf9dee57ead2d0e85a75cc5ce1191329
anghelina-semionov <62342202+anghelina-semionov@users.noreply.github.com>
2020-12-06 17:20:02 +0200

    Создание БД в SQL Server.

---
+++ DreamVoyageDB.sql
@@ -0,0 +1,23 @@
+-- Удаление БД
+DROP DATABASE DreamVouyageDB
+GO
+-- Создание БД
+CREATE DATABASE DreamVouyageDB
+GO
+-- Удалить таблицу
+--DROP TABLE DreamVouyageDB.dbo.DVUsers
+--GO
+---------------------------------------
+SELECT * FROM DreamVouyageDB.dbo.DVUsers
+GO
+SELECT * FROM DreamVouyageDB.dbo.DVUserDatas
+GO
+SELECT * FROM DreamVouyageDB.dbo.Tours
+GO
+SELECT * FROM DreamVouyageDB.dbo.UserTours
+GO
+-----------------------------------------
+-- Удалить кортежи
+--TRUNCATE TABLE DreamVouyageDB.dbo.UserTours
+--GO
+
